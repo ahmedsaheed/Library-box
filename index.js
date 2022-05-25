e22fd6fa83b3e9366f0e582d573a61bebc7940f9
Ahmed Saheed <87912847+ahmedsaheed@users.noreply.github.com>
2022-05-25 02:24:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ import request from 'superagent';
 const {
     GIST_ID: gistId,
     GH_TOKEN: githubToken,
-    GOOGLE_justFinished_UID: googlejustFinishedUID,
-    GOOGLE_justFinished_KEY: googlejustFinishedKey,
+    GOOGLE_BOOKS_UID: googlejustFinishedUID,
+    GOOGLE_BOOKS_KEY: googlejustFinishedKey,
 } = process.env;
   
-const octokit = new Octokit({
-    auth: "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"
-});
-
-
 async function main() {
     const wrap = wordwrap(58);
   
@@ -29,7 +24,7 @@ async function main() {
         
         async function justRead() {
           try {
-            var recent = await request.get('https://www.googleapis.com/books/v1/users/101611817084658660916/bookshelves/4/volumes?&key=AIzaSyDNMnPGw3yUzfIwUnH3PR4oMJiP-PecbhU');
+            var recent = await request.get('https://www.googleapis.com/books/v1/users/${googlejustFinishedUID}/bookshelves/4/volumes?&key=${googlejustFinishedKey}');
             await delay();
             return recent.body;
           } catch(error) {
@@ -39,7 +34,7 @@ async function main() {
 
         async function currentlyRead() {
           try {
-            var current = await request.get('https://www.googleapis.com/books/v1/users/101611817084658660916/bookshelves/3/volumes?&key=AIzaSyDNMnPGw3yUzfIwUnH3PR4oMJiP-PecbhU');
+            var current = await request.get('https://www.googleapis.com/books/v1/users/${googlejustFinishedUID}/bookshelves/3/volumes?&key=${googlejustFinishedKey}');
             await delay();
             return current.body;
           } catch(error) {
@@ -62,12 +57,12 @@ async function main() {
           console.log(justReadAuthor);
 
         const currentlyReading = justReadTitle && justReadAuthor
-        ? `Recently Finished: ${justReadTitle.split(':'[0])} \n By ${justReadAuthor}\n`
-        : `I haven't read recently.\n`
+            ? `Recently Finished: ${justReadTitle.split(':'[0])} \n By ${justReadAuthor}\n`
+            : `I haven't read recently.\n`
 
         const recentlyRead = currentTitle && currentAuthor
-      ? `Currently Reading: ${currentTitle.split(':')[0]} \nBy ${currentAuthor}`
-      : `I am not reading anything at the moment.\n`
+          ? `Currently Reading: ${currentTitle.split(':')[0]} \nBy ${currentAuthor}`
+          : `I am not reading anything at the moment.\n----------------------------------------------------`
         await updateGist([wrap(recentlyRead), wrap(currentlyReading)]);
         })();
     }catch(error){
@@ -83,11 +78,11 @@ async function updateGist(readingStatus) {
   let gist;
   try {
     const octokit = new Octokit({
-      auth: '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'
+      auth: githubToken
     })
     
     gist = await octokit.request('GET /gists/{gist_id}', {
-      gist_id: 'be4aa7e90f6d85a3255f4e0ed32d1a35'
+      gist_id: gistId
       
     }
     )
@@ -106,7 +101,7 @@ async function updateGist(readingStatus) {
 
   try {
     octokit.request('PATCH /gists/{gist_id}',{
-      gist_id: "be4aa7e90f6d85a3255f4e0ed32d1a35",
+      gist_id: gistId,
       files: {
         [filename]: {
           filename,
